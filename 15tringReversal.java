b9ae6bd0059b79c28e883712ac7ce549de1aa342
LOKESHWARI-ops <lokeshwarimanoharan37@gmail.com>
2025-05-23 15:57:59 +0530

    Update

---
+++ 15tringReversal.java
@@ -0,0 +1,6 @@
+import java.util.*;
+public class StringReversal {
+    public static void main(String[] args) {
+        System.out.println(new StringBuilder(new Scanner(System.in).nextLine()).reverse());
+    }
+}
