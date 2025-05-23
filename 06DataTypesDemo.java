9c501d99e8b140308759f713e94ea76b1cc787dd
LOKESHWARI-ops <lokeshwarimanoharan37@gmail.com>
2025-05-23 15:52:42 +0530

    Update

---
+++ 06DataTypesDemo.java
@@ -0,0 +1,13 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 1000000000L;
+        float f = 10.5f;
+        double d = 10.5;
+        char c = 'A';
+        boolean bool = true;
+        System.out.println("byte: "+b+"\nshort: "+s+"\nint: "+i+"\nlong: "+l+"\nfloat: "+f+"\ndouble: "+d+"\nchar: "+c+"\nboolean: "+bool);
+    }
+}
