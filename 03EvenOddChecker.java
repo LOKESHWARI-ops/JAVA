d59822d081a9de5b1a2164d4c06875fd60f03da7
LOKESHWARI-ops <lokeshwarimanoharan37@gmail.com>
2025-05-23 15:50:24 +0530

    Update

---
+++ 03EvenOddChecker.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextInt()%2==0?"Even":"Odd");
+    }
+}
