0c8f59b95f3e40cd3a54909cda4499826bcb0add
LOKESHWARI-ops <lokeshwarimanoharan37@gmail.com>
2025-05-23 15:59:38 +0530

    Update

---
+++ 18nheritanceExample.java
@@ -0,0 +1,8 @@
+class Animal{void makeSound(){System.out.println("Animal sound");}}
+class Dog extends Animal{void makeSound(){System.out.println("Bark");}}
+public class InheritanceExample {
+    public static void main(String[] args) {
+        new Animal().makeSound();
+        new Dog().makeSound();
+    }
+}
