2eb2498035c28977717d72e068def6b535d7069e
LOKESHWARI-ops <lokeshwarimanoharan37@gmail.com>
2025-05-23 15:53:31 +0530

    Update

---
+++ 07TypeCastingDemo.java
@@ -0,0 +1,6 @@
+public class TypeCastingDemo {
+    public static void main(String[] args) {
+        double d = 100.04;
+        System.out.println("Double: "+d+"\nLong: "+(long)d+"\nInt: "+(int)d);
+    }
+}
